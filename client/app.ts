6a1458a40fc789155a85d01fc0ea5ac64ca340c6
phund <itghost90@gmail.com>
2016-05-08 16:05:10 +0700

    Update packages use for angular-2.0.0-rc.1

--- client/app.ts
+++ client/app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import 'zone.js/dist/zone';
-import {Component} from 'angular2/core';
+import {Component} from '@angular/core';
 import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {Parties} from '../collections/parties';
 import {Mongo} from "meteor/mongo";
