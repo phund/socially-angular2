8d618ad9ed9177892a9bdbd1b0023f6dc856464d
phund <itghost90@gmail.com>
2016-05-15 09:22:58 +0700

    step 6

--- client/imports/party-details/party-details.ts
+++ client/imports/party-details/party-details.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { RouteParams } from '@angular/router-deprecated';
+import { RouteParams, RouterLink } from '@angular/router-deprecated';
 import { Parties } from '../../../collections/parties.ts';
-import { RouterLink }  from '@angular/router-deprecated';
+import { FormBuilder, ControlGroup, Validators, Control } from '@angular/common';
  
 @Component({
   selector: 'party-details',
@@ -12,8 +12,29 @@ export class PartyDetails {
   party: Object;
  
   constructor(params: RouteParams) {
-    var partyId = params.get('partyId');
+    let partyId = params.get('partyId');
+    let fb = new FormBuilder();
+
     this.party = Parties.findOne(partyId);
     console.log(this.party);
+
+    this.partyForm = fb.group({
+      _id: [partyId],
+      name: ['', Validators.required],
+      description: [''],
+      location: ['', Validators.required]
+    });
+  }
+
+  saveParty(party) {
+    console.log("save");
+    console.log(party);  
+    let pateyId = Parties.update(party._id, {
+      $set: {
+        name: party.name,
+        description: party.description,
+        location: party.location
+      }
+    });
   }
 }
