c6ba38caad2ce66a2b563efaa01f8497c6bb6afb
phund <itghost90@gmail.com>
2016-05-15 07:43:07 +0700

    Add form

---
+++ client/imports/parties-form/parties-form.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import {Component} from 'angular2/core';
+ 
+@Component({
+  selector: 'parties-form',
+  templateUrl: '/client/imports/parties-form/parties-form.html'
+})
+export class PartiesForm {
+  
+}
